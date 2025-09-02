f0ef61ea096f778e0cc3662aa4bc313771028216
nguyendacson <dacson1822003@gmail.com>
2025-09-02 19:32:47 +0700

    change source code

--- src/tasks/task1/Type.kt
+++ src/tasks/task1/Type.kt
@@ -1,4 +1,5 @@
 package com.example.test.tasks.task1
+
 private var orderNumber: Int = 0;
 val list = listOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
 var changeList = mutableListOf<Int>(1, 2, 3, 4, 5, 6, 7);
@@ -6,26 +7,73 @@ val set = setOf(1, 2, 3, 4, 5, 5, 6, 7, 8, 9, 10);
 val mapList = mutableMapOf("name" to 18, "name1" to 19, "name2" to 20);
 
 
-// Type Data
-val number1 = 12
-val numberAdDtype: Int = 12
-var numberAddType: Int = 12
-
-/// Integer (Byte, Short, Int, Long)
-val numberByte: Byte = 12 //(-128,127)
-val numberShort: Short = 32760 //(-32768,32767)
-val myNum1: Int = 5 //(-2147483648,2147483647)
-val myLong: Long = 2147483648 //(-9223372036854775808,9223372036854775807)
-
-val myDouble: Double = 5.1
-val myLetter: Char = 'S'
-val myBoolean: Boolean = true
-val myString: String = "Nguyen Dac Son"
-
-
-
 fun main() {
-    println(myLong::class.simpleName)
+// Type Data
+    val number1 = 12
+    val numberAdDtype: Int = 12
+    var numberAddType: Int = 12
+
+// toType want convert ex: toInt() , toByte()
+    val type1: Int = 12;
+    val type2: Double = type1.toDouble();
+    println("type after convert Int to Double $type2")
+
+    /// Integer (Byte, Short, Int, Long)
+    val numberByte: Byte = 12 //(-128,127)
+    val numberShort: Short = 32760 //(-32768,32767)
+    val myNum1: Int = 5 //(-2147483648,2147483647)
+    val myLong: Long = 2147483648 //(-9223372036854775808,9223372036854775807)
+    val myLong1: Long = 100L //(-9223372036854775808,9223372036854775807)
+
+    /// Point Type
+    val myFloat: Float = 3.14F
+    val myFloat1: Float = 12E4F  //(120000.0)
+    val Double: Double = 12.0933233
+
+    /// Boolean
+    val myBoolean: Boolean = true
+
+    /// Character
+    //// \t(tab) , \n(new line),
+
+
+    val myLetter: Char = 'S'
+    val myString: String = "Nguyen Dac Son"
+    val myString1: String = "Nguyen Dac Son1"
+
+    println("type data position 0 on my String = ${myString[0]}")
+    println("length String: " + myString.length)
+    println(myString.uppercase())
+    println(myString.lowercase())
+    val compare: Int = myString.compareTo(myString1)
+    println(compare) //(if identical => return 0 else => return -1)
+    //// search on String
+    println(myString.indexOf("Dac"))  // 7 location start Dac
+    println(myString.indexOf("Da1c"))  // -1 because "Da1c" not find on myString
+    println(myString.plus(myString1))
+    println("my name is $myString")
+
+    /// Array
+    val myArrayOf = arrayOf("12A3", "12A4", "12A5")
+    println(myArrayOf[0] + " size: " + myArrayOf.size )
+    /// change value myArrayOf[0]
+    myArrayOf[0] = "12A10"
+    println(myArrayOf[0])
+    /// check find on array
+    if ("12A3" in myArrayOf) println("success") else println("not found")
+    /// Loop array
+    for (x in myArrayOf) println(x)
+
+
+// Operator
+    /// Arithmetic(số học): +, += , - , -=, *, *= , %, %= , ++, --, /, /=
+    /// Comparison(so sánh): ==, != , > , < , >= , <=, ===(phân vùng bộ nhớ)
+    /// Logic &&(and) ||(or) !(not)
+
+
+//    println(myLong::class.simpleName)
+//    println(myLong1::class.simpleName)
+//    println(myFloat1)
 //    OddNumber().oddNumber(list)
 //    OddNumber().oddNumber1(list)
 //
@@ -46,13 +94,13 @@ fun main() {
 }
 
 //In ra các số lẻ.
-private class OddNumber{
+private class OddNumber {
 
-     fun oddNumber(list: List<Int>) {
+    fun oddNumber(list: List<Int>) {
         println("list is odd numbers:" + list.filter { it % 2 != 0 })
         println("----------------------------------------------------------------------")
-        for (i in list){
-            if (i % 2 != 0){
+        for (i in list) {
+            if (i % 2 != 0) {
                 println("odd numbers order$orderNumber: $i")
                 orderNumber++
             }
@@ -62,7 +110,7 @@ private class OddNumber{
     fun oddNumber1(list: List<Int>) {
         println("----------------------------------------------------------------------")
         list.forEachIndexed { index, value ->
-            if(value % 2 != 0){
+            if (value % 2 != 0) {
                 println("odd numbers order$index: $value");
             }
         }
@@ -70,11 +118,11 @@ private class OddNumber{
 }
 
 //Tính tổng các số chẵn.
-private class SumEvenNumber{
+private class SumEvenNumber {
 
     fun sumEvenNumber1(list: List<Int>) {
         var sum = list.filter { it % 2 == 0 }.sum();
-        var sum1 = list.filter { it % 2 == 0 }.reduce {number, it -> number + it }
+        var sum1 = list.filter { it % 2 == 0 }.reduce { number, it -> number + it }
         var sum2 = list.filter { it % 2 == 0 };
         sum2.forEach { println("number $it") }
         println("sum of even numbers: $sum")
@@ -84,8 +132,8 @@ private class SumEvenNumber{
 }
 
 //Tạo list mới chứa bình phương của tất cả phần tử.
-private class ListCopy{
-    fun listCopy(list: List<Int>){
+private class ListCopy {
+    fun listCopy(list: List<Int>) {
         var listNew = list.map { value -> value * value }
         var listNew1 = list.filter { it % 2 == 0 }.map { value -> value * value }
         println("new List: $listNew")
